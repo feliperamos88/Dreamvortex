d772dec9ffe86a4db774f9ad85a9e0dbbc9eb35a
Felipe Ramos <129113618+fliperamos88@users.noreply.github.com>
2024-04-29 21:23:33 -0400

    Update server/config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,34 +2,35 @@ import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-let sequelize;
-if (process.env.DATABASE) {
-  sequelize = new Sequelize(process.env.DATABASE, { logging: false });
-} else {
-  if (process.env.NODE_ENV === 'test') {
-    sequelize = new Sequelize(createTestDBURL(), {
-      logging: false,
-      dialectOptions: {
-        multipleStatements: true,
-      },
-    });
-  } else {
-    sequelize = new Sequelize(createTestDBURL(), {
-      logging: false,
-      dialectOptions: {
-        multipleStatements: true,
-      },
-    });
-  }
-}
-
-function createTestDBURL() {
+function createDB_URI() {
   const DB_NAME = process.env.DB_NAME;
   const DB_USER = process.env.DB_USER;
   const DB_HOST = process.env.DB_HOST;
   const DB_PASSWORD = process.env.DB_PASSWORD;
   const DB_PORT = process.env.DB_PORT;
   return `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+}
+
+let sequelize;
+
+if (process.env.NODE_ENV === 'test') {
+  sequelize = new Sequelize(process.env.DB_URI_TEST, {
+    logging: false,
+    dialectOptions: {
+      multipleStatements: true,
+    },
+  });
+} else {
+  if (process.env.DB_URI) {
+    sequelize = new Sequelize(process.env.DATABASE, { logging: false });
+  } else {
+    sequelize = new Sequelize(createDB_URI(), {
+      logging: false,
+      dialectOptions: {
+        multipleStatements: true,
+      },
+    });
+  }
 }
 
 export { sequelize };
