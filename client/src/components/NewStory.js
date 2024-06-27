fb13c747199b92149565613c5ef559b494c2e5e1
Felipe Ramos <129113618+fliperamos88@users.noreply.github.com>
2024-06-27 16:50:00 -0400

    Update NewStory.js

--- client/src/components/NewStory.js
+++ client/src/components/NewStory.js
@@ -73,7 +73,7 @@ const NewStory = ({ newGame, setNewGame }) => {
 
   return (
     <>
-      <div className="mt-3">
+      <div className="mt-3" style={{ margin: auto }}>
         {newGame && (
           <div
             style={
